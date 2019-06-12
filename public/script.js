840e6fa74849e666bab6cde064ba1c155c3e0333
Muhsinur Rahman Chowdhury <crmuhsin@gmail.com>
2019-06-12 10:57:47 +0600

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,132 @@
+const fetchData = async (asset) => {
+  try {
+    const res = await fetch(`/api/v1/${asset}`);
+    if (res.status !== 200) {
+      console.log(res);
+    }
+    const res_1 = await res.json();
+    const p = res_1[0].cpu_used;
+    return p;
+  }
+  catch (error) {
+    return console.log(error);
+  }
+}
+
+const fetchAllLocations = async (cont, asset) => {
+  try {
+    return Highcharts.chart(cont, {
+
+      chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+      },
+
+      title: {
+        text: asset
+      },
+
+      pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+          backgroundColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+            stops: [
+              [0, '#FFF'],
+              [1, '#333']
+            ]
+          },
+          borderWidth: 0,
+          outerRadius: '109%'
+        }, {
+          backgroundColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+            stops: [
+              [0, '#333'],
+              [1, '#FFF']
+            ]
+          },
+          borderWidth: 1,
+          outerRadius: '107%'
+        }, {
+          // default background
+        }, {
+          backgroundColor: '#DDD',
+          borderWidth: 0,
+          outerRadius: '105%',
+          innerRadius: '103%'
+        }]
+      },
+
+      // the value axis
+      yAxis: {
+        min: 0,
+        max: 100,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+          step: 2,
+          rotation: 'auto'
+        },
+        title: {
+          text: 'km/h'
+        },
+        plotBands: [{
+          from: 0,
+          to: 60,
+          color: '#55BF3B' // green
+        }, {
+          from: 60,
+          to: 80,
+          color: '#DDDF0D' // yellow
+        }, {
+          from: 80,
+          to: 100,
+          color: '#DF5353' // red
+        }]
+      },
+
+      series: [{
+        name: 'Speed',
+        data: [0],
+        tooltip: {
+          valueSuffix: ' km/h'
+        }
+      }]
+
+    },
+      // Add some life
+      function (chart) {
+        if (!chart.renderer.forExport) {
+          setInterval(async function () {
+            var point = chart.series[0].points[0]
+            p = await fetchData(asset);
+            console.log(point['y']);
+            point['y'] = p;
+            point.update();
+
+          }, 3000);
+        }
+      });
+  }
+  catch (error) {
+    return console.log(error);
+  }
+};
+
+fetchAllLocations('container1', 'server1');
+fetchAllLocations('container2', 'pc1');
